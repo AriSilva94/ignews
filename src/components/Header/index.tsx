9c8e9d06791d19cc85ffb3ad8a889133e63e2168
AriSilva94 <ariovaldo.bsjunior@gmail.com>
2021-08-31 20:19:56 -0300

    implementa pagina home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/Image'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
@@ -5,7 +6,12 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="Logo ignews" />
+        <Image
+          src="/images/logo.svg"
+          alt="Logo ignews"
+          height={100}
+          width={100}
+        />
         <nav>
           <a className={styles.active} href="#">Home</a>
           <a href="#">Posts</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import Head from 'next/Head'
+import Image from 'next/Image'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          height={500}
+          width={500}
+        />
+      </main>
     </>
   )
 }
